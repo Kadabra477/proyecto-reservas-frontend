bffaba09c1a90df0646a65b0dac1573b6ddd6838
Hernan <hernandimichele477@gmail.com>
2025-05-25 19:29:18 -0300

    fix: revertido a process.env.REACT_APP_API_URL para CRA

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
 });
-
 
 api.interceptors.response.use(
   (response) => response,
