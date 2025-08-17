6788804453b18ace5dc9b82c319bba2ab83a7ca6
Kadabra477 <hernandimichele477@gmail.com>
2025-08-16 22:47:56 -0300

    arreglo complejodetalle

--- src/components/Complejos/ComplejoDetalle/ComplejoDetalle.jsx
+++ src/components/Complejos/ComplejoDetalle/ComplejoDetalle.jsx
@@ -5,6 +5,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './ComplejoDetalle.css'; 
+import api from '../../../api/axiosConfig';
 
 const placeholderImage = '/imagenes/default-complejo.png';
 
