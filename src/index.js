f8b46672b78750767c7b6ebbcfd1c53a8044ea61
Kadabra477 <hernandimichele477@gmail.com>
2025-08-29 15:48:51 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// frontend/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Importa App.css aquí para que los estilos globales se apliquen primero
 import './App.css';
-// Importa Google Material Symbols CSS
-// Esta línea carga la fuente de iconos de Google, permitiendo usar los <span className="material-symbols-outlined">icon_name</span>
-import 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
+
+// ¡Línea eliminada! La carga de la fuente se moverá a index.html.
+// import 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
 
 // Ya no importamos Font Awesome si todos los iconos han sido reemplazados por Material Symbols
 // import '@fortawesome/fontawesome-free/css/all.min.css';
